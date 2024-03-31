abb481c6a2f1c4a0e8d833b2b8dfe119e956b274
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-03-31 16:44:24 +0900

    feat: 당첨 금액 상수값 추가

--- src/main/java/lotto/common/Constant.java
+++ src/main/java/lotto/common/Constant.java
@@ -8,4 +8,12 @@ public class Constant {
 
     // 구매 금액
     public static final Integer PRICE_DIVIDER_NUMBER = 1000;
+
+    // 당첨 금액
+    public static final Integer WIN_LOTTO_THREE_NUMBER = 5000;
+    public static final Integer WIN_LOTTO_FOUR_NUMBER = 50000;
+    public static final Integer WIN_LOTTO_FIVE_NUMBER = 1500000;
+    public static final Integer WIN_LOTTO_FIVE_NUMBER_WITH_BONUS = 30000000;
+    public static final Integer wIN_LOTTO_SIX_NUMBER = 2000000000;
+
 }
