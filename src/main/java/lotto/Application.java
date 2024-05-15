d7a838650ad4f5a7e48ab3d85fab00b8a7c15059
WhitePiano <startoflateness@gmail.com>
2024-05-15 16:53:07 +0900

    feat: buy lotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.domain.Money;
+import lotto.domain.Payment;
+
 public class Application {
   public static void main(String[] args) {
-    // TODO: 프로그램 구현
+    try {
+      Payment payment = new Payment(Money.fromUser());
+      System.out.println(payment);
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
+    }
   }
 }
