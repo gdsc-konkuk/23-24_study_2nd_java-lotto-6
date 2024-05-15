7d6aadad2595a056dbc085d47aa27f0bc34cb708
WhitePiano <startoflateness@gmail.com>
2024-05-15 23:28:21 +0900

    refactor: refine-structure

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,54 +1,60 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-  public static final Money PRICE = new Money(1_000);
   public static final int NUM_LOWER = 1;
   public static final int NUM_UPPER = 45;
   public static final int LEN = 6;
+  public static final Money PRICE = new Money(1_000);
 
   private final List<Integer> numbers;
-
-  public static Lotto rand() {
-    return new Lotto(Randoms.pickUniqueNumbersInRange(Lotto.NUM_LOWER, Lotto.NUM_UPPER, Lotto.LEN));
-  }
 
   public Lotto(List<Integer> numbers) {
     // make sure the numbers can be sorted.
     List<Integer> mutable = new ArrayList<>(numbers);
-    Collections.sort(mutable);
 
+    Collections.sort(mutable);
     validate(mutable);
     this.numbers = mutable;
   }
 
-  private void validate(List<Integer> numbers) {
-    if (numbers.size() != Lotto.LEN) {
-      throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이뤄져야 합니다.");
-    }
-
-    for (Integer num : numbers) {
-      if (num < Lotto.NUM_LOWER || num > Lotto.NUM_UPPER) {
-        // TODO
-        throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+  public Boolean has(int target) {
+    for (Integer number : this.numbers) {
+      if (number == target) {
+        return true;
       }
     }
 
-    Collections.sort(numbers);
-    for (int i = 0; i < numbers.size() - 1; i++) {
-      if (numbers.get(i).equals(numbers.get(i + 1))) {
-        throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
-      }
-    }
+    return false;
   }
 
   @Override
   public String toString() {
     return this.numbers.toString();
+  }
+
+  private void validate(List<Integer> numbers) {
+    if (numbers.size() != Lotto.LEN) {
+      throw new IllegalArgumentException(
+          String.format("[ERROR] 로또 번호는 %d개의 숫자로 이뤄져야 합니다.", Lotto.LEN));
+    }
+
+    for (Integer num : numbers) {
+      if (num < Lotto.NUM_LOWER || num > Lotto.NUM_UPPER) {
+        throw new IllegalArgumentException(
+            String.format(
+                "[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.", Lotto.NUM_LOWER, Lotto.NUM_UPPER));
+      }
+    }
+
+    // numbers are already sorted in constructor
+    for (int i = 0; i < numbers.size() - 1; i++) {
+      if (numbers.get(i).equals(numbers.get(i + 1))) {
+        throw new IllegalArgumentException("[ERROR] 로또 번호는 중복이 없어야 합니다.");
+      }
+    }
   }
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,26 +1,21 @@
 package lotto.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.times;
 
 class LottoTest {
   @Nested
-  class create {
+  class constructor {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void byOverSize() {
@@ -39,14 +34,14 @@ class LottoTest {
 
     @DisplayName("로또 번호가 1보다 작거나 45보다 크면 예외가 발생한다.")
     @ParameterizedTest
-    @MethodSource("generateOutOfRangeNumbers")
-    void byOutOfRangedNumbers(List<Integer> numbers) {
-      assertThatThrownBy(() -> new Lotto(numbers))
+    @MethodSource("generateOutRange")
+    void byOutRanged(List<Integer> outRanged) {
+      assertThatThrownBy(() -> new Lotto(outRanged))
           .hasMessage("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
           .isInstanceOf(IllegalArgumentException.class);
     }
 
-    private static Stream<Arguments> generateOutOfRangeNumbers() {
+    private static Stream<Arguments> generateOutRange() {
       return Stream.of(
           Arguments.of(List.of(1, 2, 3, 4, 5, 0)),
           Arguments.of(List.of(1, 2, -3, 4, 5, 8)),
@@ -55,39 +50,37 @@ class LottoTest {
   }
 
   @Nested
-  class rand {
-    @DisplayName("로또를 랜덤하게 생성할 수 있다.")
-    @RepeatedTest(5)
-    void byRandom() {
+  class has {
+    @DisplayName("숫자를 포함하고 있다면 `true`를 반환한다.")
+    @Test
+    void truePositive() {
+      // given
+      Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
       // when
-      Lotto lotto = Lotto.rand();
+      Boolean result = lotto.has(3);
 
       // then
-      assertThat(lotto).isInstanceOf(Lotto.class);
+      assertThat(result).isTrue();
     }
 
-    @DisplayName("로또를 camp.nextstep.edu.missionutils.Randoms를 활용해서 랜덤하게 생성한다.")
+    @DisplayName("숫자를 포함하고 있지 않다면 `false`를 반환한다.")
     @Test
-    void byPickUniqueNumbersInRange() {
-      try (MockedStatic<Randoms> mockedRandoms = Mockito.mockStatic(Randoms.class)) {
-        // given
-        mockedRandoms
-            .when(() -> Randoms.pickUniqueNumbersInRange(1, 45, 6))
-            .thenReturn(List.of(1, 2, 3, 4, 5, 6));
+    void trueNegative() {
+      // given
+      Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
-        // when
-        Lotto lotto = Lotto.rand();
+      // when
+      Boolean result = lotto.has(7);
 
-        // then
-        mockedRandoms.verify(() -> Randoms.pickUniqueNumbersInRange(1, 45, 6), times(1));
-        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
-      }
+      // then
+      assertThat(result).isFalse();
     }
   }
 
   @Nested
   class toString {
-    @DisplayName("정보를 문자열로 바꿀 수 있다.")
+    @DisplayName("문자열로 바꿀 수 있다.")
     @Test
     void convertToString() {
       // given
