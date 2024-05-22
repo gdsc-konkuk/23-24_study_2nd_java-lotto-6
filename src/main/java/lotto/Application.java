51abd6f71cb574dbfbc9f9ffac6d8d4e9d42f69b
WhitePiano <startoflateness@gmail.com>
2024-05-22 18:01:16 +0900

    feat: Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,18 +1,33 @@
 package lotto;
 
+import lotto.domain.BonusNumber;
 import lotto.domain.Draw;
 import lotto.domain.Money;
 import lotto.domain.Payment;
+import lotto.domain.Result;
+import lotto.domain.WinningNumbers;
+
+import java.util.function.Supplier;
 
 public class Application {
   public static void main(String[] args) {
-    try {
-      Payment payment = new Payment(Money.fromUser());
-      System.out.println(payment);
+    Money principalAmount = tillSuccess(Money::fromUser);
+    Payment payment = tillSuccess(() -> new Payment(principalAmount));
+    System.out.println(payment);
 
-      Draw draw = Draw.fromUser();
-    } catch (Exception ex) {
-      System.out.println(ex.getMessage());
+    WinningNumbers winningNumbers = tillSuccess(WinningNumbers::fromUser);
+    Draw draw = tillSuccess(() -> new Draw(winningNumbers, BonusNumber.fromUser()));
+    Result result = new Result(draw, payment);
+    System.out.println(result);
+  }
+
+  public static <T> T tillSuccess(Supplier<T> func) {
+    while (true) {
+      try {
+        return func.get();
+      } catch (Exception ex) {
+        System.out.println(ex.getMessage());
+      }
     }
   }
 }
